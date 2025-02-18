d1629e8375ce4a176b145ceaa51ac4ce1c1c189b
Hanna_Kokhan <anna.shkarupa2102@gmail.com>
2025-02-19 01:44:00 +0200

    modified files

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -21,7 +21,7 @@ export function fetchImages(query) {
           messageColor: '#ffffff',
           iconColor: '#fff',
           messageSize: '16px',
-          iconUrl: './img/bi_x-octagon.svg',
+          iconUrl: '../img/bi_x-octagon.svg',
           transitionIn: 'bounceInLeft',
           position: 'topRight',
           displayMode: 'replace',
